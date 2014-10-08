edfd47fa6db4d7dfe2b2810612eb9e5115ca7e0d
monkeywrong <paulgregorybyrne@gmail.com>
2014-10-08 22:51:12 +0100

    Added content-jumbotron template

--- wp-content/themes/hideout/footer.php
+++ wp-content/themes/hideout/footer.php
@@ -10,12 +10,14 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer container" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'hideout' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'hideout' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'hideout' ), 'Hideout', '<a href="http://www.alternativestofighting.com" rel="designer">Paul Byrne</a>' ); ?>
-		</div><!-- .site-info -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'hideout' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'hideout' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'hideout' ), 'Hideout', '<a href="http://www.alternativestofighting.com" rel="designer">Paul Byrne</a>' ); ?>
+			</div><!-- .site-info -->
+		</div>	
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
