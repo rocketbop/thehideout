93b596996c94517d20e1c1a6ef8fc739e129f6d7
monkeywrong <paulgregorybyrne@gmail.com>
2014-10-15 20:15:03 +0100

    Added Angularjs

--- wp-content/themes/hideout/functions.php
+++ wp-content/themes/hideout/functions.php
@@ -113,6 +113,14 @@ function hideout_scripts() {
 
 	wp_enqueue_script( 'hideout-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
+	wp_enqueue_script( 'angular-js', get_template_directory_uri() . '/angular/angular.js', false, NULL);
+
+	wp_enqueue_script( 'the-hideout-app', get_template_directory_uri() . '/angular/thehideoutapp.js');
+
+	wp_enqueue_script( 'event-controller', get_template_directory_uri() . '/angular/controllers/eventController.js', false, NULL);
+
+
+
 	wp_enqueue_script( 'hideout-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.2.0', true );
--- wp-content/themes/hideout/header.php
+++ wp-content/themes/hideout/header.php
@@ -7,7 +7,7 @@
  * @package Hideout
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> ng-app="theHideoutApp">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
