dd3009cd5cf3c168455b6e3bdececd5af1955280
monkeywrong <paulgregorybyrne@gmail.com>
2014-10-16 14:37:17 +0100

    Added hook to include advanced custom fields in the WP REST API

--- wp-content/themes/hideout/functions.php
+++ wp-content/themes/hideout/functions.php
@@ -142,7 +142,33 @@ add_action('wp_print_styles', 'load_fonts');
 
 add_action( 'wp_enqueue_scripts', 'hideout_scripts' );
 
+	/**
+	 * PLUGINS
+	**/
+
+// Extend custom fields plugin to allow time
+
+add_action('acf/register_fields', 'my_register_fields');
+
+function my_register_fields()
+{
+    include_once('/wp-content/plugins/acf-date_time_picker/acf-date_time_picker.php');
+}
+
 remove_filter('the_content', 'wpautop');
+
+/* Add custom fields to REST API
+ * RESPECT to: https://gist.github.com/rileypaulsen
+*/
+
+function wp_api_encode_acf($data,$post,$context){
+	$data['meta'] = array_merge($data['meta'],get_fields($post['ID']));
+	return $data;
+}
+ 
+if( function_exists('get_fields') ){
+	add_filter('json_prepare_post', 'wp_api_encode_acf', 10, 3);
+}
 
 /**
  * Implement the Custom Header feature.
