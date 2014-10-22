fae17843afc9be636d560a435abc0b8914258d66
monkeywrong <paulgregorybyrne@gmail.com>
2014-10-22 21:13:47 +0100

    Added new module structure for front page

---
+++ wp-content/themes/hideout/templates/content-musicmodule.php
@@ -0,0 +1,40 @@
+<div id="musicmodule">
+  <div class="row">
+    <div class="col-md-12">
+      <h1>MUSIC</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="row">
+
+        <div class="col-md-4">
+        Image
+        </div>
+        <div class="col-md-8">
+        Soul of Life
+        </div>
+        
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <h2>Music at the Hideout</h2>
+          <p>The Hideout has been supporting live music for years.</p>
+          <p>The atmosphere is great, and the music is too.</p>
+        </div>
+        
+      </div>
+      
+    </div>
+    <div class="col-md-8 upcoming-act">
+      <h4>NEXT:</h4>
+      <h1>BILL SHANKLY</h1>
+      <h5>14 12 2014</h5>
+
+
+      
+    </div>
+    
+  </div>
+
+</div>
