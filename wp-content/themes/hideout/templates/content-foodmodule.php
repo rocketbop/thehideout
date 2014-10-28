11bfdf6d44590e400be10eee0c72249dc097787c
monkeywrong <paulgregorybyrne@gmail.com>
2014-10-28 16:37:30 +0000

    Added News and Twitter Section for front Page

---
+++ wp-content/themes/hideout/templates/content-foodmodule.php
@@ -0,0 +1,114 @@
+<!-- This will become a template for angular which can be used for a number of sections -->
+
+
+<div class="food-module">
+
+  <div class="container">
+    <div class="row">
+        <div class="module-header col-md-4">
+          <h1>Food</h1>
+        </div>
+        <div class=" food-module-header col-md-8">
+          <h3>Live Events at <strong>The Hideout</strong></h3>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="row">
+
+          <div class="food-module-icon col-md-4">
+          Image
+          </div>
+          <div class="food-module-byline col-md-8">
+          <h3>Soul of Life</h3>
+          </div>
+          
+        </div>
+        <div class="row">
+          <div class="food-module-blurb col-md-12">
+            <h2>food at The Hideout</h2>
+            <p>The Hideout has been supporting live food for years.</p>
+            <p>Hashtag kitsch single-origin coffee artisan gentrify, tattooed kogi biodiesel Tumblr beard retro Intelligentsia. Godard ethical umami semiotics.</p>
+            <p>Slow-carb scenester swag, banjo meh Tumblr Godard Helvetica.</p>
+          </div>
+          
+        </div>
+        
+      </div>
+      <div class="col-md-8 food-module-upcoming-event-maxi">
+
+        <h4>NEXT UP:</h4>
+        <h1>U2</h1>
+        <h5>14 12 2014</h5>
+
+
+        
+      </div>
+      
+    </div>
+    <div class="row">
+      <div class='food-module-upcoming-banner col-md-4'>
+        <h2>
+          THEN:
+        </h2>
+        
+      </div>
+      <div class="food-module-upcoming-event-mini col-md-8">
+        <div class="row">
+          <div class="unit col-md-5">
+            <div class="row">
+              <div class="dark-text col-md-3">
+                <h3>Event: </h3>
+                <h4>Date: </h4>
+                <h4>Time: </h4>
+              </div>
+              <div class="col-md-9">
+                <h3>Snow Patrol</h3>
+                <h4>18 January 2015</h4>
+                <h4 class="dark-text">8PM</h4>
+              </div>
+            </div>
+
+          </div>
+          <div class="unit col-md-4 medium-background-color">
+            <div class="row">
+              <div class="dark-text col-md-3">
+                <h4>Event: </h4>
+                <h5>Date: </h5>
+                <h5>Time: </h5>
+              </div>
+              <div class="col-md-9">
+                <h4>The Frames</h4>
+                <h5>21 January 2015</h5>
+                <h5 class="dark-text">9PM</h5>
+              </div>
+
+            </div>
+          </div>
+          <div class="unit col-md-3">
+            <div class="row">
+              <div class="dark-text col-md-4">
+                <h4>Event: </h4>
+                <h5>Date: </h5>
+                <h5>Time: </h5> 
+              </div>
+              <div class="col-md-8">
+                <h4>Led Zeppelin</h4>
+                <h5>28 March 2015</h5>
+                <h5 class="dark-text">11:30PM</h5> 
+              </div>
+
+            </div>
+            
+          </div>
+        </div>
+
+
+        
+      </div>
+      
+    </div>
+  </div>
+
+
+</div>
---
+++ wp-content/themes/hideout/templates/content-newsmodule.php
@@ -0,0 +1,65 @@
+<div class="news-module">
+  
+
+<div class="container">
+  <div class="row">
+  
+    <div class="news-section col-md-8">
+      <h1>WHAT'S HAPPENING</h1>
+
+      <div class="item item-1">
+        <div class="row">
+          <div class="image-div col-md-3">
+            <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
+          </div>
+          <div class="col-md-3">
+           <h3>News Item</h3>
+          </div>
+          <div class="col-md-6">
+            <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="item item-odd">
+        <div class="row">
+          <div class="col-md-3">
+            <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
+          </div>
+          <div class="col-md-3">
+           <h3>News Item</h3>
+          </div>
+          <div class="col-md-6">
+            <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="row">
+          <div class="col-md-3">
+            <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
+          </div>
+          <div class="col-md-3">
+           <h3>News Item</h3>
+          </div>
+          <div class="col-md-6">
+            <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="col-md-4">
+      <h1 class="text-center">TWITTER</h1>
+      <a class="twitter-timeline" href="https://twitter.com/paulbyrne" height="300" data-widget-id="527117394250964992">Tweets by @paulbyrne</a>
+      <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+      </script>
+    </div>
+    
+  </div>
+</div>
+
+</div>
