2e400c6dfd61372eaa2fc41d6fa8576c381c0ecb
monkeywrong <paulgregorybyrne@gmail.com>
2014-11-04 16:14:06 +0000

    Added script to set the spash screen height to the window height

--- wp-content/themes/hideout/js/front-page-scripts.js
+++ wp-content/themes/hideout/js/front-page-scripts.js
@@ -1,11 +1,23 @@
 
 jQuery(document).ready(function($) {
+
+  // hover transformations on the News section
   $('.news-section div.item').hover(
     function() {
-      $(this).find('img').addClass('hovered');
+      $(this).addClass('hovered');
     },
     function() {
-      $(this).find('img').removeClass('hovered');
+      $(this).removeClass('hovered');
     });
-});
 
+  // set the height of the welcome spash to the inner height of the window
+  setSplashHeight(); // Set the initial size of the splash
+  $(window).resize(setSplashHeight); //Resize the splash on window resize
+
+  function setSplashHeight () {
+    var windowHeight = $(window).get(0).innerHeight;
+    $('.front-page-top-background').css('height', windowHeight);
+    // alert("Hello");
+  };
+
+});
