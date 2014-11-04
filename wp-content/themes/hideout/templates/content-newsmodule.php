693a37fc895cd7576665b47ba674142f331d4884
monkeywrong <paulgregorybyrne@gmail.com>
2014-11-04 13:21:58 +0000

    Redesigned Recent News Section and new colour scheme

--- wp-content/themes/hideout/templates/content-newsmodule.php
+++ wp-content/themes/hideout/templates/content-newsmodule.php
@@ -4,11 +4,12 @@
 <div class="container">
   <div class="row">
   
-    <div class="news-section col-md-8">
+    <div class="news-section  col-md-8">
+      <div class="section-title">
       <h1>WHAT'S HAPPENING</h1>
+      </div>
 
-      <div class="item item-1">
-        <div class="row">
+      <div class="row item item-1">
           <div class="image-div col-md-3">
             <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
           </div>
@@ -19,38 +20,53 @@
           <div class="col-md-6">
             <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
           </div>
-        </div>
       </div>
 
-      <div class="item item-odd">
-        <div class="row">
-          <div class="col-md-3">
-            <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
-          </div>
-          <div class="col-md-3">
-           <h3>News Item</h3>
-           <h5>30 September 2014</h5>
-          </div>
-          <div class="col-md-6">
-            <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
-          </div>
-        </div>
-      </div>
 
-      <div class="item">
-        <div class="row">
-          <div class="col-md-3">
-            <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
-          </div>
-          <div class="col-md-3">
-           <h3>News Item</h3>
-           <h5>19 September 2014</h5>
-          </div>
-          <div class="col-md-6">
-            <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
+      <div class="row">
+
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="row item item-bottom">
+
+              <div class="col-md-6 text-center">
+                <div class="image-div ">
+                    <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
+                  </div>
+                <h3>News Item</h3>
+                <h5>19 October 2014</h5>
+
+              </div>
+
+
+              <div class="col-md-6">
+                <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
+              </div>
           </div>
         </div>
+        
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="row item item-bottom">
+
+              <div class="col-md-6 text-center">
+                <div class="image-div ">
+                    <?php echo "<img src='" . get_template_directory_uri() . "/images/news-item.png'>" ?>
+                  </div>
+                <h3>News Item</h3>
+                <h5>19 October 2014</h5>
+
+              </div>
+
+
+              <div class="col-md-6">
+                <p>Artisan heirloom farm-to-table banjo, chambray squid skateboard +1 3 wolf moon Pinterest bicycle rights fashion axe cornhole. Drinking vinegar iPhone blog direct trade next level. Photo booth chambray ...</p>
+              </div>
+          </div>
+        </div>
+
       </div>
+      
+
+
 
     </div>
 
