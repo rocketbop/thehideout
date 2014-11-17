2b12431eacfae785252a9c4b5a563b1d8c303e0e
monkeywrong <paulgregorybyrne@gmail.com>
2014-11-17 17:19:47 +0000

    Event Page layout

--- wp-content/themes/hideout/my-templates/events-page.php
+++ wp-content/themes/hideout/my-templates/events-page.php
@@ -42,40 +42,61 @@ get_header(); ?>
 
       <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-md-offset-1">
         <div class="row">
-          <div class="col-md-3">
+          <div class="column-filter">
+            <div class="col-md-2">
          
-            <h1 class="text-center">Category</h1>
-            <a ng-click="selectCategory()"
-              class="btn btn-block btn-default btn-lg">All</a>
-              <!-- The unique filter will mean categories with more than one event will not be generated in duplicate -->
-            <a ng-click="selectCategory(event.category)" ng-repeat="event in data.events | orderBy:'category' | unique:'category'"  class=" btn btn-block btn-default btn-lg" ng-class="getCategoryClass(event.category)">{{event.category}}</a>  
+              <h4 class="text-center">Filter</h4>
+              <a ng-click="selectCategory()"
+                class="btn btn-block btn-default btn-lg">All</a>
+                <!-- The unique filter will mean categories with more than one event will not be generated in duplicate -->
+              <a ng-click="selectCategory(event.category)" ng-repeat="event in data.events | orderBy:'category' | unique:'category'"  class=" btn btn-block btn-default btn-lg" ng-class="getCategoryClass(event.category)">{{event.category}}</a>  
 
+            </div>
           </div>
-          <div class="col-md-7">
-            
-              <h1>{{selectedCategory}} Events</h1>
-              <div ng-repeat="event in data.events | orderBy: '-date_of_event' | filter:categoryFilterFn | range:selectedPage:pageSize">
+          <div class="column-main">
+            <div class="col-md-7">
+              
+                <h3>{{selectedCategory}} Events</h3>
+                <div ng-repeat="event in data.events | orderBy: '-date_of_event' | filter:categoryFilterFn | range:selectedPage:pageSize">
+                  <div class="row ">
+                    <div class="col-sm-6 col-md-6 col-lg-6 no-padding-right">
+                      <div class="board left-board">
+                        <h3>{{event.event_name}}</h3>
+                      </div>  
+                    </div>
+                    <div class=" col-sm-6 col-md-6 col-lg-6 no-padding-left">
+                      <div class=" board right-board no-padding-left">
+                         <h3>{{event.date_of_event | date: 'EEEE, d MMMM'}}</h3>
+                       </div>
+                    </div>
+                  </div>
 
-                <!-- <p><strong>Date Created </strong>{{event.date | date:'EEEE, dd MM yyyy, h:mm'}}</p> -->
-                <p><strong>Date of Event: </strong>{{event.date_of_event | date: 'medium'}}</p>
-                <p><strong>Name of Event: </strong>{{event.event_name}}</p>
-                <p><strong>Category: </strong>{{event.category}}</p>
-                <img src="{{event.event_image.url}}">
-                <p>{{event.content}}</p>
-                <!-- Save to use below for content entered through WP backend -->
-                <div ng-bind-html="event.event_description"></div>
+                  
+                 
 
-              </div>
+                  
+                  <img src="{{event.event_image.url}}">
+                  <h4>{{event.date_of_event | date: 'h:mma, EEEE, d MMMM yyyy'}}, {{event.category}}.</h4>
+                  <!-- <p>{{event.content}}</p> -->
+                  <!-- Save to use below for content entered through WP backend -->
+                  <div ng-bind-html="event.event_description"></div>
 
-              <div class="pull-right btn-group">
-                  <a ng-repeat=
-                     "page in data.events | filter:categoryFilterFn | pageCount:pageSize"
-                     ng-click="selectPage($index + 1)" class="btn btn-default"
-                     ng-class="getPageClass($index + 1)">
-                      {{$index + 1}}
-                  </a>
-              </div>
-           </div>
+                </div>
+
+                <div class="pull-right btn-group">
+                    <a ng-repeat=
+                       "page in data.events | filter:categoryFilterFn | pageCount:pageSize"
+                       ng-click="selectPage($index + 1)" class="btn btn-default"
+                       ng-class="getPageClass($index + 1)">
+                        {{$index + 1}}
+                    </a>
+                </div>
+             </div>
+            <!-- </div>  -->
+
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+              <?php get_sidebar(); ?>
+            </div>
          </div> <!-- row -->
        </div> <!-- page body -->
     </div> <!-- row -->
