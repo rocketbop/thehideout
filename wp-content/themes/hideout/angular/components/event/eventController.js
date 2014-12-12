66d2821f94d31c03ad4a96d1b43e20a89cfd0d2b
monkeywrong <paulgregorybyrne@gmail.com>
2014-12-12 14:10:17 +0000

    Formatting and clean up

--- wp-content/themes/hideout/angular/components/event/eventController.js
+++ wp-content/themes/hideout/angular/components/event/eventController.js
@@ -11,10 +11,8 @@ angular.module("theHideoutApp")
       for (var i = 0; i < $scope.data.events.length; i++) {
         $scope.data.events[i].date_of_event = $scope.data.events[i].date_of_event * 1000;
       };
-     
      console.log($scope.data.events);
     });
-
 
   // FILTER BY CATEGORY
 
@@ -23,7 +21,6 @@ angular.module("theHideoutApp")
     $scope.selectedCategory = "All";
     
     $scope.selectCategory = function (newCategory) {
-
       selectedCategory = newCategory;
       $scope.selectedPage = 1;
       // Go to page 1 on new category selection
@@ -31,9 +28,8 @@ angular.module("theHideoutApp")
       if (newCategory == undefined) {
         $scope.selectedCategory = "All";
       } else {
-      $scope.selectedCategory = selectedCategory;
+        $scope.selectedCategory = selectedCategory;
       }
-
     }; 
 
     $scope.selectPage = function (newPage) {
@@ -42,6 +38,7 @@ angular.module("theHideoutApp")
     }
 
     $scope.categoryFilterFn = function (event) {
+
       // This will return false if both conditions are false, true otherwise.
       return  selectedCategory == null ||
               event.category == selectedCategory;
@@ -60,8 +57,11 @@ angular.module("theHideoutApp")
       jQuery('html,body').animate({scrollTop: jQuery("#" + id).offset().top},'slow');
     }
 
+  // FILTERS
 
     $scope.futureEvents = function (event) {
+
+      // check that the date of the input is now or later
       var today = new Date().getTime();
        if (event.date_of_event >= today) {
         return true;
@@ -71,13 +71,13 @@ angular.module("theHideoutApp")
     }
     
   // PAGINATION
+
     $scope.selectedPage = 1; // Initial, or on page reload
     $scope.pageSize = eventListPageCount;
 
   // SINGLE POSTS
 
     $scope.getSinglePostArrayNumber = function () {
-
       for (var i = 0; i < $scope.data.events.length; i++) {
         if ($scope.data.events[i].ID == $scope.singlePostID) {
           var arrayNumber = i;
@@ -101,7 +101,6 @@ angular.module("theHideoutApp")
     } else {
       // console.log("single post ID not received");
     }
-
   }]);
 
 
--- wp-content/themes/hideout/angular/components/main/mainController.js
+++ wp-content/themes/hideout/angular/components/main/mainController.js
@@ -7,13 +7,10 @@ angular.module("theHideoutApp")
   $scope.singlePostCategory = '';
   $scope.singlePostID = '';
   
-
   // called by ng-init in templates/content-singlepost.php
   $scope.getSinglePostData = function (postCategory, postID) {
-
     $scope.singlePostCategory = postCategory;
     $scope.singlePostID = postID;
-    // console.log($scope.singlePostCategory);
   }
   
   $scope.sidebarUrl = templateDirectory + "sidebar.php";
@@ -22,91 +19,63 @@ angular.module("theHideoutApp")
   $scope.eventPartial = templateDirectory + "angular/partials/singlepostevent.php";
   $scope.newsPartial = templateDirectory + "angular/partials/singlepostnews.php";
 
-
-
-
   $scope.getScreenHeight = function () {
     var screenHeight = 0;
     screenHeight = $window.innerHeight;
-    // console.log("Screenheight:"+screenHeight);
     return screenHeight;
   }
 
   $scope.getPageHeaderHeight = function () {
-
     var pageHeaderHeight = 0;
     var pageHeaders = jQuery('.page-header');
-
     jQuery(pageHeaders).each(function () {
       pageHeaderHeight += jQuery(this).height();
     })
-    // console.log("PageHeaderHeight:"+ pageHeaderHeight);
     return pageHeaderHeight;
-
   }
 
   $scope.getNavHeight = function () {
-
     var navHeight = 0;
     var nav = jQuery('div.navbar-container');
-
     navHeight = jQuery(nav).height();
-    // console.log("NavHeight:"+navHeight);
-
     return navHeight;  
-
   }
 
   $scope.getBannerHeight = function () {
-
     var bannerHeight = 0;
     var screenHeight = $scope.getScreenHeight();
     var pageHeaderHeight = $scope.getPageHeaderHeight();
     var navHeight = $scope.getNavHeight();
-
-    // console.log(screenHeight);
-    // console.log(pageHeaderHeight);
-    // console.log(navHeight);
-
     bannerHeight = screenHeight - (navHeight + pageHeaderHeight);
-    // console.log("BannerHeight:"+bannerHeight);
     return bannerHeight;
-
   }
 
   $scope.getPanelHeight = function () {
-
     var panelHeight = 0;
     var panel = jQuery('div#blurb');
-
-    // panelHeight = jQuery(panel).height();
     panelHeight = document.getElementById('blurb').clientHeight;
-      // console.log("PanelHeight:"+panelHeight);
     return panelHeight;
-
-
   }
 
   $scope.getPanelMargin = function () {
+
     // get the margin-top to be applied to keep divs vertically centered in the banner
-    var bannerHeight = 0;
-    var panelHeight = 0;
-    var panelMargin = 0;
-    var navHeight = 0;
-    var nonPanelBannerHeight = 0;
+    var bannerHeight,
+        panelHeight,
+        panelMargin,
+        navHeight,
+        nonPanelBannerHeight = 0;
 
     bannerHeight = $scope.getBannerHeight();
     panelHeight = $scope.getPanelHeight();
     navHeight = $scope.getNavHeight();
-
     nonPanelBannerHeight = bannerHeight - panelHeight;
     panelMargin = (nonPanelBannerHeight / 2) + navHeight;
-    // console.log("PanelMargin:"+panelMargin);
     return panelMargin;
-
   }
 
    $scope.login = function() {
+
       // From now on you can use the Facebook service just as Facebook api says
       Facebook.login(function(response) {
         // Do something with response.
@@ -114,18 +83,9 @@ angular.module("theHideoutApp")
       });
     };
 
-    Facebook.getLoginStatus(function(response) {
-      console.log(response);
-    });
-
-    $scope.login();
-
     $scope.data = {};
     $scope.data.myTestURL= 'http://www.theguardian.com';
-    console.log($scope.data.myTestURL);
-
-  
-  
+    
   }]);
 
 
