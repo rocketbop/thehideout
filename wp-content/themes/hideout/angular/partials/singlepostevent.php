474c22b8ecaca416bdf34087feb7669d696918a3
monkeywrong <paulgregorybyrne@gmail.com>
2015-01-02 20:26:14 +0000

    Responsive styles: Single Page Event

--- wp-content/themes/hideout/angular/partials/singlepostevent.php
+++ wp-content/themes/hideout/angular/partials/singlepostevent.php
@@ -2,16 +2,16 @@
   
   <div class="container-fluid">
     <div class="row">
-      <div class="background-container col-xs-12 col-sm-12 col-md-12 col-lg-12" data-my-div-height="navbar" minus-header="false">
+      <div class="background-container col-xs-12 col-sm-12" data-my-div-height="navbar" minus-header="false">
       </div>
     </div>
 
     <div class="row page-header page-header-top">
-      <div class="page-inner col-xs-10 col-sm-10 col-md-10 col-lg-10 col-md-offset-1">
+      <div class="page-inner col-md-10 col-md-offset-1">
           <div class="row">
-            <div class="col-xs-1 col-sm-1 col-md-9 col-lg-9">
+            <div class="col-xs-12 col-md-9">
               <div class="row">
-                <div class="col-xs-1 col-sm-1 col-md-9 col-lg-9 col-md-offset-3">
+                <div class="col-md-9 col-md-offset-3">
                   <h1>{{singlePost.event_name}}</h1>
                 </div>
               </div>  
@@ -22,11 +22,11 @@
   </div> <!-- fluid container -->
     <div class="container-fluid">
     <div class="row page-header page-header-bottom">
-      <div class="page-inner col-xs-10 col-sm-10 col-md-10 col-lg-10 col-md-offset-1">
+      <div class="page-inner col-md-10 col-md-offset-1">
           <div class="row">
-            <div class="col-xs-1 col-sm-1 col-md-9 col-lg-9">
+            <div class="col-md-9">
               <div class="row">
-                <div class="col-xs-1 col-sm-1 col-md-9 col-lg-9 col-md-offset-3">
+                <div class="col-md-9 col-md-offset-3">
                   <h4>{{singlePost.date_of_event | date: 'h:mma, EEEE, d MMMM yyyy'}}</h4>
                 </div>
               </div>  
@@ -37,22 +37,21 @@
   </div> <!-- fluid container -->
 
   <div class="container-fluid">
-    <div class="row">
-      <div class="page-inner col-xs-10 col-sm-10 col-md-10 col-lg-10 col-md-offset-1">
+    <div class="row module-body">
+      <div class="module-body-inner col-md-10 col-md-offset-1">
         <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+          <div class="col-xs-12 col-sm-12 col-md-9">
             <img class="featured-image" src="{{singlePost.featured_image.attachment_meta.sizes.eventboard.url}}">
-
             <div class="row">
-              <div class="col-xs-1 col-sm-1 col-md-3 col-lg-3">
+              <div class="col-md-3 col-lg-3">
                 <div class="social-buttons">
                 <div class="row">
-                  <div class="col-xs-1 col-sm-6 col-md-6 col-lg-6">
+                  <div class="col-sm-6 col-md-6 col-lg-6">
                    <!-- Below used as test while on local host
                   Substitute with singlepost.link when moving onto live server. -->
                     <div class="fb-like" data-href="{{data.myTestURL}}" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
                   </div>
-                  <div class="col-xs-1 col-sm-6 col-md-6 col-lg-6">
+                  <div class="col-sm-6 col-md-6 col-lg-6">
                     <a href="https://twitter.com/share" class="twitter-share-button" data-via="paulbyrne">Tweet</a>
                     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                   </div>
@@ -61,18 +60,15 @@
                 </div>
                 <div class="date-published">
                   <h6>Posted: {{singlePost.date | date: 'EEEE, d MMMM yyyy, h:mm' }}</h6>
-                </div>
-                
-                
+                </div>          
               </div>
-              <div class="col-xs-1 col-sm-1 col-md-9 col-lg-9">
+              <div class="col-md-9 col-lg-9">
                 <div ng-bind-html="singlePost.event_description"></div>
                 <img class="separator" ng-src="{{templateDirectory}}images/design/guitar-separator.png">
               </div>
             </div>
           </div>
-
-          <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 no-padding-left">
+          <div class="hidden-xs hidden-sm col-md-3 col-lg-3 no-padding-left">
             <div ng-include="sidebarUrl"></div>
           </div>
         </div> <!-- row -->
--- wp-content/themes/hideout/my-templates/events-page.php
+++ wp-content/themes/hideout/my-templates/events-page.php
@@ -8,14 +8,10 @@ get_header(); ?>
 <div id="primary" class="content-area secondary-page events events-page visible-links visible-links-cherry" ng-controller="eventCtrl">
   <div class="container-fluid">
     <div class="row">
-        <div class="background-container col-xs-12 col-sm-12 col-md-12 col-lg-12" data-my-div-height="full" minus-header="true">
-
+      <div class="background-container col-xs-12 col-sm-12 col-md-12 col-lg-12" data-my-div-height="full" minus-header="true">
         <div class="row">
           <div class="col-sm-10 col-md-10 col-lg-10 col-sm-offset-1">
             <div class="row">
-              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                
-              </div>
               <div id="blurb" class="blurb col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-6 col-md-6" data-my-vertical-center ext-function="getPanelMargin()">
                 <h2>We Love Music</h2>
                 <p>We care about music at The Hideout.</p>
@@ -23,8 +19,8 @@ get_header(); ?>
             </div>
           </div>
         </div>
-        </div>
       </div>
+    </div>
     <div class="row page-header page-header-top">
       <div class="page-inner col-md-10 col-lg-10 col-md-offset-1">
           <div class="row">
@@ -40,41 +36,30 @@ get_header(); ?>
   <div class="container-fluid">
     <div class="row page-header page-header-bottom">
       <div class="page-inner col-md-10 col-md-offset-1">
-                  <div class="row">
+        <div class="row">
           <div class="col-md-7 col-lg-7 col-md-offset-2">
-
-          <h4>Get the details on all upcoming music and events at The Hideout.</h4>
-
+            <h4>Get the details on all upcoming music and events at The Hideout.</h4>
           </div>
-          </div>
+        </div>
       </div>  <!-- page header -->
     </div> <!-- row -->
   </div> <!-- fluid container -->
 
   <div id ="entries" class="container-fluid">
-    <div class="row page-body ">
-       <div class=" body-outer col-xs-1 col-sm-1 col-md-1 col-lg-1">
-        
-      </div>
-
-      <div class="body-inner col-xs-10 col-sm-10 col-md-10 col-lg-10">
-          <div class="column-main">
+    <div class="row module-body">
+      <div class="module-body-inner col-md-10 col-md-offset-1">
+          <div class="row">
             <div class="col-md-9 col-lg-9">
               <div ng-include src="htmlTemplatesDirectory + 'events_section_template.html'"></div>
-
-             </div>
-            <!-- </div>  -->
-
+            </div>
             <div class="hidden-xs hidden-sm col-md-3 no-padding-left">
-            <div ng-include src="htmlTemplatesDirectory + 'event_filter_template.html'"></div>
-
+              <div ng-include src="htmlTemplatesDirectory + 'event_filter_template.html'"></div>
               <?php get_sidebar(); ?>
             </div>
          </div> <!-- row -->
        </div> <!-- page body -->
     </div> <!-- row -->
   </div> <!-- container fluid -->
-
 </div> <!-- ng controller -->
 
 <?php get_footer(); ?>
